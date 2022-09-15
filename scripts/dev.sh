b99d203ab321e28ec9f21f9e2497805a9ca2fd12
Karthik <karthikborkar+1@gmail.com>
2022-09-15 00:52:16 -0700

    ISSUE-3 & Issue-6: Provide a simple dev utility and update readme

---
+++ scripts/dev.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -e
+
+if [[ $1 == "init" ]]; then
+    echo "Init the dev setup..."
+    echo `pwd`
+    bash scripts/setup.sh
+    echo "Successfully installed dependencies.. "
+    echo "Start the app using - dev up"
+    
+elif [[ $1 == "up" ]]; then
+    echo "Building Astra app.."
+    go build
+    echo "Starting the Astra app.."
+    
+    port=$2
+    if [[ -z $port ]]; then
+        port=3000
+    fi
+    ./astra server --port=$port
+elif [[ $1 == "test" ]]; then
+    echo "Unit Testing Astra app.."
+    go test
+else
+    echo "Invalid choice. Select between dev init or dev up"
+fi
