a8aa57b8f274facfa714c9e794a0ee538f2f2e18
karthikbic1 <113502439+karthikbic1@users.noreply.github.com>
2022-09-15 00:47:35 -0700

    ISSUE-3: Base Skeleton for Golang Astra Rest Service (#5)

---
+++ cmd/server.go
@@ -0,0 +1,88 @@
+/*
+Copyright © 2022 NAME HERE <EMAIL ADDRESS>
+
+*/
+package cmd
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/spf13/cobra"
+)
+
+var port int
+
+// serverCmd represents the server command
+var serverCmd = &cobra.Command{
+	Use:   "server",
+	Short: "A brief description of your command",
+	Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		mux := CreateRouter()
+
+		log.Printf("Listening on Port: %v", port)
+		log.Fatal(http.ListenAndServe(fmt.Sprint(":", port), loggingMiddleware(mux)))
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(serverCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// serverCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	serverCmd.PersistentFlags().IntVar(&port, "port", 3000, "Port to start Astra on..")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// serverCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
+
+func CreateRouter() *mux.Router {
+	r := mux.NewRouter()
+	r.HandleFunc("/", testHandler)
+
+	return r
+}
+
+func loggingMiddleware(handler http.Handler) http.Handler {
+
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		defer log.Printf("%s - %s\n", r.Method, r.URL)
+		handler.ServeHTTP(w, r)
+	})
+}
+
+func serverHandler(handler http.Handler) http.Handler {
+
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		handler.ServeHTTP(w, r)
+	})
+}
+
+func testHandler(w http.ResponseWriter, r *http.Request) {
+	type response struct {
+		Status  string
+		Message string
+	}
+
+	resp := &response{
+		Status:  "ok",
+		Message: "Hello World!",
+	}
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(&resp)
+	return
+}
---
+++ go.mod
@@ -0,0 +1,11 @@
+module astra
+
+go 1.18
+
+require (
+	github.com/bitly/go-simplejson v0.5.0
+	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/inconshreveable/mousetrap v1.0.0 // indirect
+	github.com/spf13/cobra v1.5.0 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+)
